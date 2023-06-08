83973195750583994b7cba48680fb25f3971c6bf
Sergio Peralta <seperalta88@gmail.com>
2023-06-08 15:26:34 -0300

    Create first pages

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="bg-red-400">
+		Layout: MainLayout
+		<slot />
+	</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="bg-red-400">Test</div>
+</template>

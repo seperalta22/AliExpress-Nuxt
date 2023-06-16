60587a134e45fa7e0e8217d5eb8f3ea3e3dc230d
Sergio Peralta <seperalta88@gmail.com>
2023-06-16 02:33:35 -0300

    little update

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -12,6 +12,8 @@ let isSearching = ref(false);
 let searchItem = ref('');
 let items = ref(null);
 
+onMounted(() => (isSearching.value = false));
+
 const searchByName = useDebounce(async () => {
 	isSearching.value = true;
 	items.value = await useFetch(
